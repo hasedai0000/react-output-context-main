1674f4f5a86ce53c1e7ba70fee3e13e683ed99b3
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-06-10 23:41:01 +0900

    first commit

---
+++ src/pages/todo.jsx
@@ -0,0 +1,19 @@
+/**
+ * TodoPage
+ *
+ * @package pages
+ */
+import { TodoProvider } from "../contexts/TodoContext.jsx";
+import { TodoTemplate } from "../components/templates/TodoTemplate";
+
+/**
+ * TodoPage
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const TodoPage = () => (
+  <TodoProvider>
+    {/* TodoProvider直下のコンポーネントでコンテキストに定義した状態、ロジックをどこでも呼び出せる */}
+    <TodoTemplate />
+  </TodoProvider>
+);
